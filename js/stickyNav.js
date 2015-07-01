739e903618d1a0c447c965c8bbfcdeb683caf624
Milam <charles.milam@gmail.com>
2015-06-30 23:49:00 -0500

    Add lemonade and compass to project. Init compass for use with project.

--- js/stickyNav.js
+++ js/stickyNav.js
@@ -6,10 +6,10 @@ function stickyNav(){
   $(window).scroll(function(){
     if( $(window).scrollTop() > stickyNavTop ) {
       $('#stickynav').css({position: 'fixed', top: '0px', margin: "0px"})
-      $("#stickynav li").css({margin: "-16px 0 0 3rem"})
+      $("#stickynav li").css({margin: "0 0 0 3em"})
     } else {
-      $('#stickynav').css({position: 'static', top: '0px', margin: "-20px 0 0 0"})
-      $("#stickynav li").css({margin: "0 0 0 3rem"})
+      $('#stickynav').css({position: 'static', top: '0px', margin: "1em 0 0 3em"})
+      $("#stickynav li").css({margin: "1em 0 0 3em"})
     }
   })
 }
